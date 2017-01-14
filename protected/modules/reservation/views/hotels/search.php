c14bccab36f9016d4d753efb65df1c8c098b98ec
Masoud Gharagozlu <gharagozlu.masoud@gmail.com>
2017-01-14 20:07:32 +0330

    Fix stay time bug

--- protected/modules/reservation/views/hotels/search.php
+++ protected/modules/reservation/views/hotels/search.php
@@ -240,7 +240,7 @@ if(!isset($nextPage))
                 'itemsCssClass'=>'items overflow-fix container-fluid',
                 'template'=>'{items}',
                 'viewData'=>array(
-                    'duration'=>floor(((Yii::app()->session['outDate']-Yii::app()->session['inDate'])/(3600*24))),
+                    'duration'=>Yii::app()->session['stayTime'],
                     'country'=>(isset($country))?$country:null,
                     'searchID'=>(isset($searchID))?$searchID:null,
                     //'city'=>(isset($city))?$city:null,
